a501dfb762b599abb5114a04054bd08d02fcf73e
olesshem <95028761+olesshem@users.noreply.github.com>
2024-01-27 22:51:34 +0200

    Add files via upload

---
+++ srp.cpp
@@ -0,0 +1,9 @@
+﻿Build started at 22:43...
+1>------ Build started: Project: srp1, Configuration: Debug x64 ------
+1>C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Microsoft\VC\v170\Microsoft.CppBuild.targets(513,5): warning MSB8003: The WindowsSDKDir property is not defined. Some build tools may not be found.
+1>srp1.cpp
+1>C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.38.33130\include\yvals.h(20,10): error C1083: Cannot open include file: 'crtdbg.h': No such file or directory
+1>(compiling source file 'srp1.cpp')
+1>Done building project "srp1.vcxproj" -- FAILED.
+========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 22:43 and took 00,429 seconds ==========
