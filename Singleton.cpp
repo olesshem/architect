eb99583ea544e3f752ea2704789da8db2d4ca536
olesshem <95028761+olesshem@users.noreply.github.com>
2024-01-27 23:38:14 +0200

    Add files via upload

---
+++ Singleton.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+// Клас Singleton
+class Singleton {
+public:
+    // Статичний метод для доступу до екземпляра Singleton
+    static Singleton& getInstance() {
+        static Singleton instance; // Гарантований один екземпляр
+        return instance;
+    }
+
+    // Видалення методів копіювання та присвоєння
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+    void display() const {
+        std::cout << "I am a Singleton!" << std::endl;
+    }
+
+private:
+    // Приватний конструктор
+    Singleton() = default;
+};
+
+int main() {
+    // Отримання та використання екземпляра Singleton
+    Singleton& singletonInstance = Singleton::getInstance();
+    singletonInstance.display();
+
+    return 0;
+}
